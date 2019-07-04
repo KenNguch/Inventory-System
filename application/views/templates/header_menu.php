a93334ac26df81ebefbdc433cb4486d5cc3dc239
KenNguch <kennguch3@gmail.com>
2019-07-04 13:12:18 +0300

    Changed Index

--- application/views/templates/header_menu.php
+++ application/views/templates/header_menu.php
@@ -1,10 +1,10 @@
 <header class="main-header">
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="https://murmuring-woodland-75772.herokuapp.com/" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>ADN</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin</b></span>
+      <span class="logo-lg"><b>Kenya Forest Service ERP</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
