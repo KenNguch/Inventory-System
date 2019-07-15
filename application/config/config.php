edc6ef89dfb915404c450d2e2121371e4926dba3
KenNguch <kennguch3@gmail.com>
2019-07-15 13:16:21 +0300

    Base Url Fix

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Inventory-System-/';
+$config['base_url'] = 'https://murmuring-woodland-75772.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
