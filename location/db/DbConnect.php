9b88fb8d7a7ff1b5024413b29078a96ca872f6ba
KenNguch <kennguch3@gmail.com>
2019-08-13 12:25:59 +0300

    Last Update

--- location/db/DbConnect.php
+++ location/db/DbConnect.php
@@ -1,18 +1,19 @@
 <?php
-	class DbConnect {
-		private $host = 'us-cdbr-iron-east-02.cleardb.net';
-		    private $dbName = 'heroku_28c866c0cec52d8';
-		    private $user = 'b1ba6b6cee86e7';
-		    private $pass = '6b75b8d6';
-				
-		public function connect() {
-			try {
-				$conn = new PDO('mysql:host=' . $this->host . '; dbname=' . $this->dbName, $this->user, $this->pass);
-				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				return $conn;
-			} catch( PDOException $e) {
-				echo 'Database Error: ' . $e->getMessage();
-			}
-		}
-	}
- ?>
+class DbConnect
+{
+    private $host = 'us-cdbr-iron-east-02.cleardb.net';
+    private $dbName = 'heroku_1cb4b64cbb33b13';
+    private $user = 'b62665f49732ed';
+    private $pass = '4f426027';
+
+    public function connect()
+    {
+        try {
+            $conn = new PDO('mysql:host=' . $this->host . '; dbname=' . $this->dbName, $this->user, $this->pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            echo 'Database Error: ' . $e->getMessage();
+        }
+    }
+}
